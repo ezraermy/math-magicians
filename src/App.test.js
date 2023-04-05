fedd00c9d06079cd384acccdbc3deb4df513a2b4
Ezra <ezraermy@gmail.com>
2023-04-05 18:28:58 +0300

    Add test for operator using jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import operate from './logic/operate';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('operate', () => {
+  test('adds two numbers', () => {
+    expect(operate('3', '5', '+')).toBe('8');
+  });
+
+  test('subtract two numbers', () => {
+    expect(operate('4', '2', '-')).toBe('2');
+  });
+
+  test('multiply two numbers', () => {
+    expect(operate('5', '5', '×')).toBe('25');
+  });
+
+  test('divdide two numbers', () => {
+    expect(operate('6', '2', '÷')).toBe('3');
+  });
+
+  test('divdide by zero', () => {
+    expect(operate('6', '0', '÷')).toBe('Can\'t divide by 0.');
+  });
+
+  test('modulus operation', () => {
+    expect(operate('7', '2', '%')).toBe('1');
+  });
+
+  test('modulus operation by zero', () => {
+    expect(operate('7', '0', '%')).toBe('Can\'t find modulo as can\'t divide by 0.');
+  });
 });
